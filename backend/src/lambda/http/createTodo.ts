19875a42af2895cec8eecc8189b7a1ad6eb5b645
HXXUANNGUY <hoang.xuannguyen@coxautoinc.com>
2022-10-07 14:39:51 +0700

    super enhance backend

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -6,12 +6,44 @@ import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 import { getUserId } from '../utils';
 import { createTodo } from '../../businessLogic/todos'
 
+import { createLogger } from '../../utils/logger'
+
+const logger = createLogger("createTodo");
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    const newTodo: CreateTodoRequest = JSON.parse(event.body)
-    // TODO: Implement creating a new TODO item
-
-    return undefined
+    logger.info("## CREATE TODO ##")
+    
+    try {
+      const newTodo: CreateTodoRequest = JSON.parse(event.body)
+      
+      // TODO: Implement creating a new TODO item
+      if(newTodo.name == "" || newTodo.name == null){
+        return {
+          statusCode: 422,
+          body: JSON.stringify({
+            "message": "Task is required"
+          })
+        }
+      }
+      const user = getUserId(event);
+      const todo = await createTodo(newTodo,user)
+      logger.info("## CREATE TODO SUCCESSFULLY ##")
+      return {
+        statusCode: 201,
+        body: JSON.stringify({
+          "item": todo
+        })
+      }
+    } catch (error) {
+      logger.error("## CREATE TODO FAILED ##", { error: error.message })
+      return {
+        statusCode: 500,
+        body: JSON.stringify({
+          "message": "System errors"
+        })
+      }
+    }
+  }
 )
 
 handler.use(
--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -2,20 +2,50 @@ import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
-import { cors } from 'middy/middlewares'
+import { cors, httpErrorHandler } from 'middy/middlewares'
 
 import { getTodosForUser as getTodosForUser } from '../../businessLogic/todos'
 import { getUserId } from '../utils';
+
+import { createLogger } from '../../utils/logger'
+
+const logger  = createLogger("getTodos");
 
 // TODO: Get all TODO items for a current user
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     // Write your code here
-    const todos = '...'
+    logger.info("## GET TODOS ##")
 
-    return undefined
+    try {
+      const userId = getUserId(event)
 
-handler.use(
+      logger.info("## GET USER ID ##")
+      const todos = await getTodosForUser(userId);
+
+      logger.info("## TODOS By USER ID SUCCESSFULLY ##")
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          "items": todos
+        })
+      };
+    } catch (error) {
+      logger.error('## GET TODO FAILED: ', { error: error.message })
+      
+      return {
+        statusCode: 500,
+        body: JSON.stringify({
+          "message": "System errors"
+        })
+      }
+    }
+  })
+
+handler
+  .use(httpErrorHandler())
+  .use(
   cors({
     credentials: true
   })
